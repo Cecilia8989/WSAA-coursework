86db178e748a6258d97aefdff8775727f1c543e2
Cecilia Pastore <ceciliapastore89@gmail.com>
2024-02-04 15:03:43 +0000

    assignement 1 final first drawn

--- assignements/currentweather.py
+++ assignements/currentweather.py
@@ -1,26 +1,27 @@
+# Import necessary libraries
 import requests
 import json
-import locationtagger
 from geopy.geocoders import Nominatim
 
 # Specify my current town location coordinates
 latitude = 51.903614
 longitude = -8.468399
 
-# Initialize the Nominatim geocoder
-geoLoc = Nominatim(user_agent="GetLoc")
+# Get the city and the country from the latitude and longitude
+# Reference: https://www.geeksforgeeks.org/get-the-city-state-and-country-names-from-latitude-and-longitude-using-python/
 
-# Reverse geocode the coordinates
-location = geoLoc.reverse(str(latitude) + "," + str(longitude))
+# Initialize Nominatim API for reverse geocoding
+geolocator = Nominatim(user_agent="geoapiExercises")
+
+# Reverse geocode the coordinates to obtain the address details
+location = geolocator.reverse(f"{latitude},{longitude}")
 
 # Extract the address data
 address = location.raw['address']
 
-# Get the city
+# Get the city and country from the address
 city = address.get('city', '')
-
-# Print the city
-print('City:', city)
+country = address.get('country', '')
 
 # Construct the URL for fetching weather data based on the provided latitude and longitude
 url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m&current=temperature_2m,wind_speed_10m"
@@ -28,9 +29,6 @@ url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={lo
 # Make a request to the Open Meteo API and parse the JSON response
 response = requests.get(url)
 data = response.json()
-
-with open ("current_weather.json", 'w') as fp:
-    json.dump(data, fp)
 
 # Define keys for temperature and wind speed in the API response
 temp_value = "temperature_2m"
@@ -43,12 +41,10 @@ temp_2m = currentvalue[temp_value]
 wind_direction_10m = currentvalue[wind_value]
 
 # Print temperature and wind speed along with their respective units
-# Utilize title() to capitalize the first letter of variable names for better formatting 
-# https://www.geeksforgeeks.org/how-to-capitalize-first-character-of-string-in-python/
+# Reference: https://www.geeksforgeeks.org/how-to-capitalize-first-character-of-string-in-python/
 
-# define title to print
-title_to_print = "-- Temperature and wind speed on Ork "
-
-
-print(f'{temp_value.title()} : {temp_2m} {currentunits[temp_value]}')
-print(f'{wind_value.title()} : {wind_direction_10m} {currentunits[wind_value]}')
+# Print temperature and wind speed along with their respective units
+print("\n-- Temperature and Wind Speed --")
+print(f'   City: {city} ({country})')
+print(f'   {temp_value.title()}: {temp_2m} {currentunits[temp_value]}')
+print(f'   {wind_value.title()}: {wind_direction_10m} {currentunits[wind_value]}')
