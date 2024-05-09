1049617734d02fa12d4cc299323a431e399a389d
Cecilia Pastore <ceciliapastore89@gmail.com>
2024-05-09 02:43:46 +0100

    currentweather fixed

--- assignements/currentweather.py
+++ assignements/currentweather.py
@@ -14,16 +14,6 @@ longitude = -8.486316
 # Get the city and the country from the latitude and longitude
 # Reference: https://www.geeksforgeeks.org/get-the-city-state-and-country-names-from-latitude-and-longitude-using-python/
 
-# Initialize Nominatim API for reverse geocoding
-geolocator = Nominatim(user_agent="geoapiExercises")
-# Reverse geocode the coordinates to obtain the address details
-location = geolocator.reverse(f"{latitude},{longitude}")
-# Extract the address data
-address = location.raw['address']
-# Get the city and country from the address
-city = address.get('city', '')
-country = address.get('country', '')
-
 # Construct the URL for fetching weather data based on the provided latitude and longitude
 url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,wind_direction_10m"
 
@@ -46,6 +36,5 @@ wind_direction_10m = currentvalue[wind_value]
 
 # Print temperature and wind speed along with their respective units
 print("\n-- Temperature and Wind Direction --")
-print(f'   City: {city} ({country})')
 print(f'   {temp_value.title()}: {temp_2m} {currentunits[temp_value]}')
 print(f'   {wind_value.title()}: {wind_direction_10m} {currentunits[wind_value]}')
